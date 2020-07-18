0778432691afe00ef6c99648e7378a2792e25c53
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-18 13:24:29 +0100

    sort using size and iterator gives no error

--- app/src/main/java/com/example/algorithmskotlin/sorting.kt
+++ app/src/main/java/com/example/algorithmskotlin/sorting.kt
@@ -5,8 +5,11 @@ import java.util.*
 fun main(args:Array<String>){
     var numToSort= listOf<Int>(10,2,1,20,9,22)
 
-    for (i in numToSort.indices-1)
-        for(j in numToSort.indices-2){
+    var rangeI=numToSort.size-1
+    var rangeJ=numToSort.size-2
+
+    for (i in 0..rangeI)
+        for(j in 0..rangeJ){
             if(numToSort[j]>numToSort[j+1]){
                 Collections.swap(numToSort,j,j+1)
             }
