c836a6067171c0d50716b6b4b1c1d20259c5434a
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-07-18 13:22:39 +0100

    sort with indices gives error

--- app/src/main/java/com/example/algorithmskotlin/sorting.kt
+++ app/src/main/java/com/example/algorithmskotlin/sorting.kt
@@ -1,6 +1,16 @@
 package com.example.algorithmskotlin
 
+import java.util.*
+
 fun main(args:Array<String>){
     var numToSort= listOf<Int>(10,2,1,20,9,22)
+
+    for (i in numToSort.indices-1)
+        for(j in numToSort.indices-2){
+            if(numToSort[j]>numToSort[j+1]){
+                Collections.swap(numToSort,j,j+1)
+            }
+        }
+    print(numToSort)
 
 }
